8bea383428ff805eb5dc57859c8ba430f337357a
Adam Oubelkas <adam.oubelkas@edu-kw1c.nl>
2020-08-12 20:45:16 +0200

    Overzicht php opdrachten bijgewerkt
    Opmaak en structuur overzichtpagina voor PHP opdrachten  bijgewerkt o.b.v. huisstijl van heel de portfoliowebsite.

--- myWebPortfolio/webpaginas/mijnPortfolio/PHP/php-leerjaar1/index.php
+++ myWebPortfolio/webpaginas/mijnPortfolio/PHP/php-leerjaar1/index.php
@@ -20,21 +20,55 @@
     <head>
         <link type="text/css" rel="stylesheet" href="css/style.css">
         <meta charset="utf-8" />
-        <title>Opdrachten overzicht PHP leerjaar 1</title>
+        <title>Opdrachten overzicht PHP mbo leerjaar 1</title>
+		<script src="../../../../scripts/jquery-3.2.0.min.js"></script>
+		<script src="../../../../scripts/Script.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('header > h1').empty('*');
+                $('header > h1').css('display', 'none');
+                $('header > h1').append("Hier treft u een overzicht van alle verwijzingen naar alle in mbo leerjaar 1 uitgewerkte PHP opdrachten").fadeIn(1000);
+                $('section > nav > h3 > a').empty();
+                $('section > nav > h3 > a').css('display', 'none');
+                $('section > nav > h3 > a').append("Portfoliowebsite <br/> Adam Oubelkas").fadeIn(1000);
+				$('section > #main').animate({width: "80%", height: "100vh"}, 900);
+            });
+        </script>
     </head>
     <body>
-        <header><h1>ICT academie AO</h1></header>
-        <nav>
-            <ul>
-                <?php
-                    printOpdrachten();
-                ?>
-            </ul>
-        </nav>
-        <div id="wrapper">
-            <h2>Opdrachten PHP Leerjaar 1</h2>
-            <p>Dit is de startpagina van de website waarin ik alle opdrachten van PHP uitwerk en oplever.
-            </p>
-	    </div>
+        <header>
+            <h1></h1>
+        </header>
+        <section>
+            <nav>
+				<h3><a href="../index.html">Portfoliowebsite <br/> Adam Oubelkas</a></h3>
+                <ul>
+                    <li>
+                        <a href="../webpaginas/Wie-ben-ik.html" id="Wie-ben-ik">Wie ben ik?</a>
+                    </li>
+                    <li>
+                        <a href="../webpaginas/Mijn-opleidingen.html" id="Mijn-opleidingen">Mijn Opleidingen</a>
+                    </li>
+                    <li>
+                        <a href="../webpaginas/Mijn-portfolio.html" id="Mijn-portfolio">Mijn Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="../webpaginas/Mijn-hobby.html" id="Mijn-hobbys">Mijn Hobby's</a>
+                    </li>
+                </ul>
+            </nav>
+            <div id="main">
+				<div id="wrapper">
+					<ul>
+						<?php
+							printOpdrachten();
+						?>
+					</ul>
+				</div>
+			</div>
+        </section>
+        <footer>
+            <p>&copy 2020 Adam Oubelkas <br> Gemaakt in HTML5, CSS3, PHP7.1 en JQuery 3.2.0.</p>
+        </footer>
     </body>
 </html>
