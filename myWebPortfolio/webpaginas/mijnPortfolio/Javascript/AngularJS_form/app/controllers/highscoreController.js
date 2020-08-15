77aaed6eefcd016546ac9e1536b3d1ac528f7d14
Adam Oubelkas <adam.oubelkas@edu-kw1c.nl>
2020-08-15 22:03:17 +0200

    AngularJS algemene form - functionaliteit bijgewerkt
    *Nu worden alle gebruikersinvoer omgezet in een getal dat toegevoegd wordt aan de highscorelist.
    *AngularJS module bijgewerkt naar versie 1.8.0.
    *Duplicaties aan sleutels in AngularJS array wordt voorkomen door iedere sleutel de toegewezen arraypositie als de waarde te koppelen i.p.v. de gebruikerswaarde. Meer info: https://code.angularjs.org/1.6.4/docs/error/ngRepeat/dupes?p0=x%20in%20highscores&p1=string:4&p2=4

--- myWebPortfolio/webpaginas/mijnPortfolio/Javascript/AngularJS_form/app/controllers/highscoreController.js
+++ myWebPortfolio/webpaginas/mijnPortfolio/Javascript/AngularJS_form/app/controllers/highscoreController.js
@@ -2,13 +2,24 @@
     $scope.highscores = [],
     $scope.statusHighscore = true;
     $scope.addHighscore = function () {
-        $scope.highscores += $scope.newHighscore;
-        $scope.newHighscore = "";
-        $scope.statusHighscore = false;
+        if ($scope.newHighscore !== "") {
+            $scope.highscores += (isNaN(Number($scope.newHighscore)) ? 0 : Number($scope.newHighscore));
+            $scope.newHighscore = "";
+            $scope.statusHighscore = false;
+        }
+        else {
+            $scope.statusHighscore = true;
+        }
     },
     $scope.removeHighscore = function () {
-        $scope.highscores -= $scope.newHighscore;
-        $scope.newHighscore = "";
-        $scope.statusHighscore = false;
+        if ($scope.highscores.length > 0) {
+            $scope.highscores -= $scope.highscores[$scope.highscores.length - 1];
+            if ($scope.highscores.length <= 0) {
+                $scope.statusHighscore = true;
+            }
+        }
+        else {
+            $scope.statusHighscore = true;
+        }                 
     };
 });
