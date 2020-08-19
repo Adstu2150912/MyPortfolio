7f807861e0d6448a53f069b2a4cc14cc25101d7f
Adam Oubelkas <adam.oubelkas@edu-kw1c.nl>
2020-08-19 19:22:30 +0200

    Quizpagina volledig uitgewerkt en geoptimaliseerd
    Opmaak en functionaliteit van de Quizpagina vertoont vanaf nu geen zichtbare bugs op het gebruikersscherm.

--- myWebPortfolio/scripts/Quiz.js
+++ myWebPortfolio/scripts/Quiz.js
@@ -1,16 +1,26 @@
 $(document).ready(function () {
-    //bewaar hieronder de willekeurige getal als bepalend factor voor het selecteren van een quizvraag
-    //Hierdoor kan de gebruiker bij elke bezoek van deze quiz een andere volgorde aan vragen krijgen
-    var ranNum;
     //bewaar hieronder voor de gebruiker de huidige score 
     //waarvan de punten bijkomen of erafgaan afhankelijk van juistheid antwoorden van de gebruiker
     var huidigeScore = 0;
+
+    //Hou hieronder het aantal dat goed beantwoord is 
+    var aantalVragenGoed = 0;
+
+    //Hier wordt het aantal items in array 'vragen' bijgehouden, zodat de grootte van de deze array niet per ongeluk aangepast wordt
+    var hoogsteIndex = 4;
 
     //Onderstaande zijn alle vragen die de gebruiker moet beantwoorden in de quiz om punten voor eigen score te verzamelen
     var vragen = [vraag1 = { vraag: "Hoe heet Nintendo's nieuwste product?", antwoord: 'Nintendo Switch', optie1: 'Nintendo Swift', optie2: 'New Nintendo Wii', optie3: 'Nintendo Wii U', optie4: 'Nintendo Switch' },
     vraag2 = { vraag: "Welke computertaal is NIET front-end?", antwoord: 'PHP', optie1: 'HTML', optie2: 'JavaScript', optie3: 'PHP', optie4: 'CSS' },
     vraag3 = { vraag: "Wat is waar?", antwoord: 'Boolean is waar OF niet waar', optie1: 'Boolean is waar OF niet waar', optie2: 'False IS NIET gelijk aan False', optie3: 'True IS gelijk aan False', optie4: 'False heeft nooit geen waarde' },
     vraag4 = { vraag: "Welke component heeft een permanente opslagruimte?", antwoord: 'HDD', optie1: 'ODD', optie2: 'RAM', optie3: 'HDD', optie4: 'VRAM' }];
+
+    //bewaar hieronder de willekeurige getal als bepalend factor voor het selecteren van een quizvraag
+    //Hierdoor kan de gebruiker bij elke bezoek van deze quiz een andere volgorde aan vragen krijgen
+    var ranNum = Math.floor(Math.random() * Math.floor(hoogsteIndex));
+
+    //ID van de timer die 5 seconden wacht voordat deze quizEvent() aanroept
+    var timerID;
 
     $('header > h1').empty();
     $('header > h1').css('display', 'none');
@@ -35,37 +45,49 @@ $(document).ready(function () {
         //Als de gebruiker de spelregels begrijpt, kan de Quiz beginnen door onderstaande knop te laten klikken
         $('#spelregels > input').click(function () {
             $('#spelregels').remove();
-            $('input[name="meerkeuzevraag"]').click()
+            //Maak alvast heel het formulier leeg en vul het quizformulier volledig met de door 'ranNum' gekozen vraag en opties
+            $('form[name="quizForm"] > h3').empty();
+            $('form[name="quizForm"] > h3').append(vragen[ranNum].vraag);
+            $('form[name="quizForm"] > #optie1').val(vragen[ranNum].optie1);
+            $('form[name="quizForm"] > #optie2').val(vragen[ranNum].optie2);
+            $('form[name="quizForm"] > #optie3').val(vragen[ranNum].optie3);
+            $('form[name="quizForm"] > #optie4').val(vragen[ranNum].optie4);
+            $('form[name="quizForm"] > #lblOptie1').text(vragen[ranNum].optie1);
+            $('form[name="quizForm"] > #lblOptie2').text(vragen[ranNum].optie2);
+            $('form[name="quizForm"] > #lblOptie3').text(vragen[ranNum].optie3);
+            $('form[name="quizForm"] > #lblOptie4').text(vragen[ranNum].optie4);
+
+            $('form[name="quizForm"]').fadeIn(800);
+            timerID = setTimeout(function () { quizEvent(); }, 5000);
         });
     });
 
     //Als een vraag uit een Quiz is beantwoord, verwijs de gebruiker dan naar de volgende vraag
     //Dit gaat net zo lang door totdat alle vragen zijn beantwoord of de tijd voorbij
-    $('input[name="meerkeuzevraag"]').click(function () {
-        //Haal de waarde van de geselecteerde antwoorde op 
-        var gekozenOptie = $('input[name="meerkeuzevraag"]:checked').val();
-        //Hou hieronder het aantal dat goed beantwoord is 
-        var aantalVragenGoed = 0;
+    //Als onderstaande event actief is, dan moet de huidige timer samen met de bijbehorende event gedeactiveerd worden
+    $('input[name="meerkeuzevraag"]').click(function () { clearTimeout(timerID); quizEvent(); });
 
+    function quizEvent() {
         //Maak alvast heel het formulier leeg ter voorbereiding van de eventuele volgende vraag
         $('form[name="quizForm"] > h3').empty();
 
-        for (var i = 0; i < 4; i++) {
-            $('form[name="quizForm"] > #optie' + i).empty();
+        //Haal de waarde van de geselecteerde antwoorde op 
+        var gekozenOptie = $("input:radio[name='meerkeuzevraag']:checked").val();
+
+        if (gekozenOptie == vragen[ranNum].antwoord) {
+            huidigeScore += 10;
+            aantalVragenGoed += 1;
         }
+        else {
+            huidigeScore -= 3;
+        }
+
+        vragen.splice(ranNum, 1);
 
         //Als nog niet alle vragen door de gebruiker zijn beantwoord, laat de quiz dan doorgaan
         //Zo wel, dan is de quiz afgelopen en moet een overzicht met daarin de behaalde score aan de gebruiker getoond worden
         if (vragen.length > 0) {
-            ranNum = Math.floor((Math.random() * Math.floor(vragen.length)));
-
-            if (gekozenOptie == vragen[ranNum].antwoord) {
-                huidigeScore += 10;
-                aantalVragenGoed += 1;
-            }
-            else {
-                huidigeScore -= 3;
-            }
+            ranNum = Math.floor(Math.random() * Math.floor(hoogsteIndex -= 1));
 
             $('form[name="quizForm"] > h3').append(vragen[ranNum].vraag);
             $('form[name="quizForm"] > #optie1').val(vragen[ranNum].optie1);
@@ -77,12 +99,11 @@ $(document).ready(function () {
             $('form[name="quizForm"] > #lblOptie3').text(vragen[ranNum].optie3);
             $('form[name="quizForm"] > #lblOptie4').text(vragen[ranNum].optie4);
 
-            vragen.splice(ranNum, 1);
             $('form[name="quizForm"]').fadeIn(800);
 
             //Als meer dan 5 seconden zijn geweest voordat de gebruiker antwoord heeft gegeven, 
             // dan moet de volgende vraag getoond worden.
-            setTimeout(function () { $('input[name="meerkeuzevraag"]').click()}, 5000);
+            timerID = setTimeout(function () { quizEvent() }, 5000);
         }
         else
         {
@@ -93,13 +114,11 @@ $(document).ready(function () {
             $('#main > #resultaten').append('<h1>Dit zijn de resultaten:</h1>' + '<p>Er zijn ' + aantalVragenGoed +' vragen juist beantwoord.</p>' +
             '<p>U heeft ' + huidigeScore + ' punten in totaal verzameld over heel de Quiz.</p>' +
             '<p>U kunt de Quiz opnieuw spelen!</p> <input type="button" value="Ga verder">').fadeIn(1000).css('text-align', 'center');
+            $('#resultaten > input').click(function () {
+                location.reload(true);
+            });
         }
-    });
-
-    $('#resultaten > input').click(function () {
-        location.reload(true);
-    });
-
+    }
 });
 
 
