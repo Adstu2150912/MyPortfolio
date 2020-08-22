0664fbaae539c883a62547246c812ac38858afda
Adam Oubelkas <adam.oubelkas@edu-kw1c.nl>
2020-08-22 13:59:36 +0200

    Functionaliteit webpagina ontwerpdiagrammen bijgewerkt
    *Alle visuele ontwerpdiagrammen worden in de map 'IMG' bewaard.
    *Gebroken link verwezen naar juiste URL-adres.

--- myWebPortfolio/webpaginas/mijnPortfolio/Diagram/Script/Diagram.js
+++ myWebPortfolio/webpaginas/mijnPortfolio/Diagram/Script/Diagram.js
@@ -1,33 +1,31 @@
 ﻿$(document).ready(function () {
     $('#UML').click(function () {
-        $('#main img').attr('src', 'ASP - UML Activiteitendiagram.jpg');
+        $('#main img').attr('src', 'IMG/ASP - UML Activiteitendiagram.jpg');
         $('#main img').css('visibility', 'visible');
         $('#main img').css('width', '30%');
     });
 
     $('#Bachmanndiagram').click(function () {
-        $('#main img').attr('src', 'Bachmanndiagram.png');
+        $('#main img').attr('src', 'IMG/Bachmanndiagram.png');
         $('#main img').css('visibility', 'visible');
         $('#main img').css('width', '75%');
     });
 
     $('#cogNIAM').click(function () {
-        $('#main img').attr('src', 'cogNIAM kennisdiagram.png');
+        $('#main img').attr('src', 'IMG/cogNIAM kennisdiagram.png');
         $('#main img').css('visibility', 'visible');
         $('#main img').css('width', '50%');
     });
 
     $('#klassen').click(function () {
-        $('#main img').attr('src', 'klassendiagram.png');
+        $('#main img').attr('src', 'IMG/klassendiagram.png');
         $('#main img').css('visibility', 'visible');
         $('#main img').css('width', '50%');
     });
 
     $('#SQL').click(function () {
-        $('#main img').attr('src', '../SQL/SQL tabel KNVB voetbal.jpg');
+        $('#main img').attr('src', 'IMG/SQL tabel KNVB voetbal.jpg');
         $('#main img').css('visibility', 'visible');
         $('#main img').css('width', '75%');
     });
-
-
 })
